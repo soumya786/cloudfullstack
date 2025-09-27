d06e03266d594fa91dc8237840127e5d5a421275
soumya ranjan swain <soumyaranjanswain877@gmail.com>
2025-09-27 20:27:29 +0530

    First commit

---
+++ src/main/java/com/soumya/cloudfullstack/CloudfullstackApplication.java
@@ -0,0 +1,13 @@
+package com.soumya.cloudfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudfullstackApplication.class, args);
+	}
+
+}
